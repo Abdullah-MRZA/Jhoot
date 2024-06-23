19663d7ddb08e6b08621e54bcb3d377472141234
Abdullah Mirza <abdullahprograms@gmail.com>
2024-06-23 18:13:01 +0100

    cleaned up the main function, and added some visualisations. Also changed the expectations... (because usability is probably more important)

--- README.md
+++ README.md
@@ -4,13 +4,25 @@ A program to determine if your USB/SD card or other storage device has the capac
 A simple program to determine if a storage medium has the storage capacity that it claims to have...
 
 Features
-- Minimal, doesn't require any external dependencies (other than python)
+- ~~Minimal, doesn't require any external dependencies (other than python)~~
 - Single file, easy to carry around
-- Multithreaded, meaning your drive can work at its full capacity (for a faster test)
+- ~~Multithreaded, meaning your drive can work at its full capacity (for a faster test)~~
 - Custom fragmentation, so that any partitioning method can be checked
 
 
 If you have any ideas for extending the functionality of this program, get in touch!
 
-> [!WARNING]
-> Currently a Work In Progress!! (not ready for usage)
+## How it works
+
+Enter how many gigabytes you want to check.
+The program will then create those files, and check if they are corrupt.
+
+If they are corrupt, it will halt, and notify you that, and give you an option to continue, or terminate the program.
+
+At the end, you can see how many files were corrupted, and how many were preserved
+
+## What information can this tell me?
+
+Some USB/SD cards fake their storage, so this program makes sure that they store exactly what they say they do.
+
+There should not be any corruption whatsoever, so any corrupted files may be a sign that you need to investigate...
--- main_program.py
+++ main_program.py
@@ -1,44 +1,51 @@
-import threading
 import hashlib
+import string
+from rich import print
+from rich.progress import track
 import math
-import os
+# import random
+# import threading
+# import os
 
 
 def work_out_sha_256(text_str: str) -> str:
     return hashlib.sha256(text_str.encode("utf-8")).hexdigest()
 
 
-class Storage:
-    def __init__(self, total_file_size_GB: int):
-        print("Storing data in memory")
+def storage_analysis(total_file_size_GB: int) -> None:
+    print("Storing random data in memory...")
 
-        data = "".join(
-            str(x % 10) for x in range(1024**3)
-        )  # make 1 GB of size in memory
+    # data = "".join(
+    #     str(random.randint(0, 9)) for x in track(range(1024**3))
+    # )  # make 1 GB of size in memory
+    data = ((string.ascii_letters + string.digits) * 2)[:100] * ((1024**3) // 100)
 
-        data_sha256 = work_out_sha_256(data)
+    data_sha256 = work_out_sha_256(data)
+    total_corrupted: int = 0
 
-        for file_part in range(math.ceil(total_file_size_GB)):
-            print(f"Making file {file_part + 1}")
-            with open(f"{file_part}.jhoot", "w") as file:
-                _ = file.write(data)
+    print("Writing data to disk")
+    for file_part in track(range(math.ceil(total_file_size_GB))):
+        print(f"Making file {file_part + 1}")
+        with open(f"{file_part}.jhoot", "w") as file:
+            _ = file.write(data)
 
-            print("    Verifying produced file is correct")
-            with open(f"{file_part}.jhoot", "r") as file:
-                new_sha256 = work_out_sha_256(file.read().strip())
-                if new_sha256 != data_sha256:
-                    print("ERROR - SHA256 DOES NOT MATCH")
-                    print(f"{new_sha256}, WHEN EXPECTED {data_sha256}")
-                    print(
-                        "DATA CORRUPTION HAS OCCURED, POSSIBLY INDICATING FAULTY DRIVE"
-                    )
-                    choice = input("CONTINUE ANYWAYS? (Y/n)").lower()
-                    if choice == "n":
-                        return
+        print("    Verifying produced file is correct")
+        with open(f"{file_part}.jhoot", "r") as file:
+            new_sha256 = work_out_sha_256(file.read().strip())
+
+            if new_sha256 != data_sha256:
+                print("ERROR - SHA256 DOES NOT MATCH")
+                print(f"{new_sha256}, WHEN EXPECTED {data_sha256}")
+                print("DATA CORRUPTION HAS OCCURED, POSSIBLY INDICATING FAULTY DRIVE")
+                choice = input("CONTINUE ANYWAYS? (Y/n)").lower()
+                total_corrupted += 1
+                if choice == "n":
+                    return
+    print(f"\nProgram completed, with {total_corrupted} corrupted files")
 
 
 def main() -> None:
-    files = Storage(total_file_size_GB=int(input("How many Gigabytes to test:")))
+    storage_analysis(total_file_size_GB=int(input("How many Gigabytes to test:")))
 
 
 if __name__ == "__main__":
